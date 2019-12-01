694ce9fbdb553bdc7d1ed176aca40c06db5ead85
travismcchesney <travisj37@hotmail.com>
2019-12-01 15:33:08 -0700

    Utterly useless

--- day01/p2.rb
+++ day01/p2.rb
@@ -1,17 +1,17 @@
-def get_fuel(tot, mass)
+def get_fuel(mass)
   fuel = (mass.to_i / 3) - 2
 
   if fuel <= 0
     return 0
   end
 
-  return fuel + get_fuel(tot, fuel)
+  return fuel + get_fuel(fuel)
 end
 
 sum = 0
 
 File.open('input.txt').each do |mass|
-  sum += get_fuel(0, mass)
+  sum += get_fuel(mass)
 end
 
 puts sum
